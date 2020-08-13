9d50bb4e4b94531ac6e3debcf5736657cd130dbf
Mapple <mapplezq@gmail.com>
2020-08-13 12:44:00 +0800

    build phonebook frontend and copy to backend

---
+++ build/precache-manifest.e0b1ff0b3ba3c7fb64af674ff5a4024b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd95ec99b42cda4fe82336e8e3ceb68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49ec108ba16c7b451bd9",
+    "url": "/static/css/main.19db4f3c.chunk.css"
+  },
+  {
+    "revision": "16ff7094960d85c49953",
+    "url": "/static/js/2.d9a5f722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9a5f722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ec108ba16c7b451bd9",
+    "url": "/static/js/main.112a832d.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
